ce5fbd02560b0fae47b5e12d3119b67256d5fe77
pcosin <pcosin@hotmail.com>
2023-09-27 20:54:22 -0300

    titulo cambia sin animacion, peleando con libros

---
+++ src/data/libros.js
@@ -0,0 +1,22 @@
+ const LibrosData = {
+  1: {
+    title: "La comunidad",
+    fecha: "",
+    img: "Autor 1",
+    descripcion: "Descripción del Libro 1",
+  },
+  2: {
+    title: "Gatillo fácil",
+    fecha: "",
+    img: "Autor 2",
+    descripcion: "Descripción del Libro 2",
+  },
+  3: {
+    title: "Requiem",
+    fecha: "",
+    img: "Autor 3",
+    descripcion: "Descripción del Libro 3",
+  },
+};
+
+export default LibrosData
